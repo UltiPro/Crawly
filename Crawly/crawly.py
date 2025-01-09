892f59023235c7835db67a47a63d297c6e244117
Patryk Wójtowicz <ultiprogames@gmail.com>
2025-01-09 02:06:01 +0100

    Update crawly.py

--- Crawly/crawly.py
+++ Crawly/crawly.py
@@ -1,4 +1,4 @@
-import requests
+import random
 import time as t
 import csv
 import networkx as nx
@@ -6,7 +6,7 @@ import plotly.graph_objects as go
 
 from collections import deque
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 from datetime import datetime
 
 from fake_useragent import UserAgent
@@ -26,6 +26,8 @@ class Crawly:
         self._results = []
         self._edges = []
         self._start_time = t.time()
+
+        self._init_bool = True
 
     def start(self):
         print(
@@ -60,7 +62,12 @@ class Crawly:
             return
         print(f"{self._time(time)} | {depth}")
         queue = []
-        self._process_page(url, depth, queue)
+        try:
+            self._process_page(url, depth, queue)
+        except Exception as e:
+            print(
+                f"UNRESOLVED EXCEPTION OCCURED ... Check Internet Connection or Proxy Server.\nInfo:\n{e}"
+            )
         for link, new_depth in queue:
             if link not in self._visited:
                 self._dfs(link, new_depth)
@@ -69,62 +76,81 @@ class Crawly:
         return depth > self._max_depth or time > self._max_time
 
     def _process_page(self, url, depth, queue):
-        body_content = None
+        soup = None
         try:
-            response = requests.get(url, timeout=5)
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            with sync_playwright() as p:
-                browser = p.chromium.launch(headless=False)
-                page = browser.new_page()
+            soup = self._process_page_soap(url)
+        except Exception:
+            soup = self._process_page_soap(url, False)
 
-                # self._set_random_proxy(page)
-
-                headers = {
-                    "User-Agent": UserAgent().random,
-                    "Accept-Language": "en-US,en;q=0.9",
-                    "Referer": url,
-                }
-                page.set_extra_http_headers(headers)
-
-                page.goto(url)
-
-                body_content = page.content()
-                soup = BeautifulSoup(body_content, "html.parser")
-
-                # CAPTCHA
-                if self._is_captcha_page(soup, body_content):
-                    input("Press Enter to close the browser...")
-
-                browser.close()
-
-        except Exception as e:
-            print(f"Unresolved exception during processing '{url}': {e}")
-
-        soup = BeautifulSoup(
-            body_content if body_content else response.text, "html.parser"
-        )
         self._results.append((url, soup.get_text(separator=" ", strip=True)))
         self._visited.add(url)
         for a_tag in soup.find_all("a", href=True):
             full_url = urljoin(url, a_tag["href"])
+            parsed_url = urlparse(full_url)
             if full_url not in self._visited and full_url.startswith("http"):
-                self._edges.append((url, full_url))
-                queue.append((full_url, depth + 1))
+                if not parsed_url.path.endswith(
+                    (".jpg", ".png", ".css", ".js", ".svg")
+                ):
+                    self._edges.append((url, full_url))
+                    queue.append((full_url, depth + 1))
+
+    def _process_page_soap(self, url, not_protected=True):
+        with sync_playwright() as p:
+            browser = p.chromium.launch(
+                headless=False,
+                slow_mo=(0 if not_protected else (random.uniform(0, 2) * 1000)),
+                proxy={
+                    "server": "dc.oxylabs.io:8001",
+                    "username": "user-test1_SIc47",
+                    "password": "Test1Test1Test1_"
+                },
+                timeout=20000
+            )
+            page = browser.new_page()
+
+            # if not protected:
+            # self._set_random_proxy(page)
+
+            headers = {
+                "User-Agent": UserAgent().random,
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "Accept-Language": "pl-PL,pl;q=0.9,en-GB;q=0.8,en-US;q=0.7,en;q=0.6",
+                "Referer": "https://www.google.com/" if self._init_bool else url,
+                "Upgrade-Insecure-Requests": "1",
+            }
+            self._init_bool = False
+            page.set_extra_http_headers(headers)
+
+            page.goto(url)
+
+            if not not_protected:
+                try:
+                    cookie_button = page.wait_for_selector(
+                        "button[data-role='accept-consent']",
+                        timeout=(random.uniform(1, 4) * 1000),
+                    )
+                    cookie_button.click()
+                except Exception:
+                    pass
+
+            body_content = page.content()
+            soup = BeautifulSoup(body_content, "html.parser")
+
+            # CAPTCHA
+            if not not_protected and self._is_captcha_page(soup, body_content):
+                page.wait_for_timeout((random.uniform(1, 5) * 1000))
+                input("Press Enter to close the browser...")
+
+            browser.close()
+
+            return soup
 
     def _is_captcha_page(self, soup, body_content):
-        """
-        Funkcja do wykrywania CAPTCHA na stronie.
-        """
-        keywords = [
-            "captcha",
-            "verify",
-            "recaptcha",
-        ]  # Typowe frazy dla stron z CAPTCHA
+        keywords = ["captcha", "recaptcha", "verify"]
+
         if any(keyword in body_content.lower() for keyword in keywords):
             return True
 
-        # Sprawdzenie obecności obrazków CAPTCHA
         for img in soup.find_all("img"):
             if (
                 "captcha" in img.get("src", "").lower()
